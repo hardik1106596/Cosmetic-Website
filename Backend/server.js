6f95c623c7d73f5acbb8dcf8174591bbd1dc470d
hardik1106596 <hardiksagadhara@gmail.com>
2026-06-08 16:30:31 +0530

    long off long off long off

--- Backend/server.js
+++ Backend/server.js
@@ -29,10 +29,6 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
 });
 
 // ============================================================
